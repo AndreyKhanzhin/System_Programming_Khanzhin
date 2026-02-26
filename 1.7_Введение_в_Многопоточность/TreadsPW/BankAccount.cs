644aff3fac67d19e89d1a6fba2d7b7d46801dd36
AndreyKhanzhin <118679620+AndreyKhanzhin@users.noreply.github.com>
2026-02-26 10:34:44 +0300

    Add files via upload

---
+++ 1.7_Введение_в_Многопоточность/TreadsPW/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreadsPW
+{
+
+    public class BankAccount
+    {
+        public int balance = 1000;
+        private readonly object _locker = new object();
+        public void Withdraw(int amount)
+        {
+            lock (_locker)
+            {
+                if (amount <= balance)
+                {
+                    Thread.Sleep(10);
+                    balance -= amount;
+                    Console.WriteLine($"Списание прошло успешно. Осталось {balance}.");
+                }
+                else
+                {
+                    Console.WriteLine("ОШИБКА ОПЕРАЦИИ");
+                }
+            }
+        }
+    }
+}
---
+++ 1.7_Введение_в_Многопоточность/TreadsPW/Program.cs
@@ -0,0 +1,19 @@
+﻿using TreadsPW;
+
+class Prigram
+{
+    static void Main()
+    {
+        BankAccount account = new BankAccount();
+
+        List<Task> operations = new List<Task>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            operations.Add(Task.Run(() => account.Withdraw(150)));
+        }
+
+        Task.WaitAll(operations);
+    }
+}
+
